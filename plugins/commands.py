afc7398be792cfc99287c233a4bef8956f98a7d8
MuhammedRk <70426191+MuhammedRk@users.noreply.github.com>
2021-06-06 18:17:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -112,7 +112,7 @@ async def nxt(c, m, cb2=False):
         await m.answer()
         await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
         if not cb2:
-            await m.reply_text(m.text, reply_markup=InlineKeyboardMarkup(buttons), quote=True)
+            await m.edit_text(m.text, reply_markup=InlineKeyboardMarkup(buttons), quote=True)
         else:
             await m.answer()
             await style_buttons(c, m, cb=True)
